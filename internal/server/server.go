79da89bcd739f131b35ec62f62339defcfac12ac
qzy1233 <3206426114@qq.com>
2025-09-08 00:26:27 +0800

    实现优雅关闭

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,10 +2,16 @@ package server
 
 import (
 	"broadcas-server/internal/client"
+	"context"
+	"errors"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 var upgrader = websocket.Upgrader{}
@@ -75,21 +81,61 @@ func (s *Server) Run() {
 			for c := range s.Clients {
 				err := c.Socket.WriteMessage(websocket.TextMessage, msg)
 				if err != nil {
-					log.Printf("write error: %v", err)
+					log.Printf("write error, user %s will be removed: %v", c.Username, err)
+					// 写入失败，意味着连接可能已断开
+					// 启动一个新的 goroutine 来处理离开逻辑，以避免阻塞当前的消息广播循环
+					go func(clientToLeave *client.Client) {
+						s.Leave <- clientToLeave
+					}(c)
 				}
 			}
 		}
 	}
 }
 
+func (s *Server) broadcastServerShutdown() {
+	shutdownMsg := []byte("Server is shutting down. Disconnecting.")
+	for c := range s.Clients {
+		c.Socket.SetWriteDeadline(time.Now().Add(time.Second * 1))
+		err := c.Socket.WriteMessage(websocket.TextMessage, shutdownMsg)
+		if err != nil {
+			log.Printf("write shutdown message error: %v", err)
+		}
+		c.Socket.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	}
+}
+
 func StartServer(port string) {
 	server := NewServer()
-	addr := ":" + port
 	go server.Run()
+	addr := ":" + port
+	mux := http.NewServeMux()
+	mux.HandleFunc("/{username}", server.socketHandler)
+	httpServer := &http.Server{Addr: addr, Handler: mux}
+
+	// 监听中断信号来优雅关闭
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+		<-quit
+
+		log.Println("Shutting down server...")
+
+		// 创建一个有超时的 context
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		// 通知所有客户端服务器将要关闭
+		server.broadcastServerShutdown()
+
+		if err := httpServer.Shutdown(ctx); err != nil {
+			log.Fatalf("server shutdown error: %v", err)
+		}
+	}()
+
 	log.Printf("start server on %v", port)
-	http.HandleFunc("/{username}", server.socketHandler)
-	err := http.ListenAndServe(addr, nil)
-	if err != nil {
-		log.Fatalf("start server error: %v", err)
+	if err := httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatalf("listen %s error: %v", port, err)
 	}
+	log.Println("server exiting")
 }
